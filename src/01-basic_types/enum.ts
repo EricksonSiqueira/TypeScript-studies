091a975f58fa49a41aeacda91c72f09179973651
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2022-08-18 21:03:46 -0300

    feat: using enums

---
+++ src/01-basic_types/enum.ts
@@ -0,0 +1,13 @@
+enum Colors {
+  black = 0,
+  white = 10,
+  green = 2,
+}
+
+enum Colors {
+  red = 1,
+  blue = 3,
+}
+// whe can bombine enum's using the same name in it
+
+console.log(Colors);
