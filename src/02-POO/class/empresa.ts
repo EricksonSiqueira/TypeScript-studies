d81a3c2a46fca30a9dd1e8e4c2c15c4410cf9180
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2022-08-29 18:28:30 -0300

    feat: using classes with ts

---
+++ src/02-POO/class/empresa.ts
@@ -0,0 +1,31 @@
+export class Empresa {
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  addColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  showColaborador(): void {
+    this.colaboradores.forEach((colaborador) => console.log(colaborador));
+  }
+}
+
+export class Colaborador {
+  constructor(public readonly nome: string, private readonly cpf: string) {}
+}
+
+const empresa1 = new Empresa('Udemy', '11.1111111/00001-11');
+const colaboradores1 = new Colaborador('Erickson', '123.456.789-90');
+const colaboradores2 = new Colaborador('Ana', '123.456.789-90');
+const colaboradores3 = new Colaborador('Adriana', '123.456.789-90');
+empresa1.addColaborador(colaboradores1);
+empresa1.addColaborador(colaboradores2);
+empresa1.addColaborador(colaboradores3);
+empresa1.showColaborador();
