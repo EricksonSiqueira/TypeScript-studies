5a320a970c7fb321b775c403b730f410187925e2
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2022-09-05 20:33:34 -0300

    feat: null operator trigger on null and undefined

--- src/03-advanced_types/optional-chaning-and-null-operator.ts
+++ src/03-advanced_types/optional-chaning-and-null-operator.ts
@@ -10,4 +10,8 @@ const documento: Documento = {
 };
 
 //optional channing
-console.log(documento.date?.toDateString());
+console.log(documento.date?.toDateString() ?? 'Date does not exist'); // ?? only triggers on null and undefined
+console.log(undefined ?? 'Undefined trigger');
+console.log(null ?? 'Null trigger');
+console.log(false ?? 'False does not trigger');
+console.log(`` ?? 'empety string does not trigger');
